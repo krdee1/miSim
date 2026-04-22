6cb6dabcb56765924471f03d855a2a902734d866
Kevin D <krdee1@gmail.com>
2026-04-22 08:16:20 -0700

    plotted 0 tilt SNR over range

--- @rfSensor/plotParameters.m
+++ @rfSensor/plotParameters.m
@@ -18,11 +18,19 @@ function f = plotParameters(obj)
     % Sample SINR (SNR) function by distances, tilts
     % using SINR method with no other transmitters defined is equivalent to SNR
     s_x = obj.sensorPerformance(d, t); % don't define other sensors
+    s_x = reshape(s_x, size(d_mg));
 
     % Plot resultant sigmoid curves
-    f = figure;
+    figure;
+    plot(d_values.', s_x(repmat((t_values == 0).', 1, size(d_values, 2))), "LineWidth", 2);
     grid("on");
-    surf(d_mg, t_mg, reshape(s_x, size(d_mg)));
+    title("SNR vs Distance at 0 tilt");
+    xlabel("Distance (m)");
+    ylabel("SNR (dB)");
+
+    figure;
+    surf(d_mg, t_mg, s_x);
+    grid("on");
     title("SNR vs Distance and Tilt");
     xlabel("Distance (m)");
     ylabel("Tilt (deg)");
